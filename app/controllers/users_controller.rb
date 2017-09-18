eaf28988d83050e2ef40458e4078dac1ae86e3d0
Tin Nguyen <upkey1010@gmail.com>
2017-09-18 11:08:43 +0700

    Done Chapter 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,34 @@
 class UsersController < ApplicationController
-  def new; end
+  def show
+    @user = User.find_by(id: params[:id])
+    if @user.blank?
+      flash[:danger] = t(".error_message")
+      redirect_to action: "index"
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new user_params
+    if @user.save
+      flash[:success] = t("welcome")
+      redirect_to @user
+    else
+      render "new"
+    end
+  end
+
+  def index
+    @users = User.sort_by_name
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+      :password_confirmation)
+  end
 end
